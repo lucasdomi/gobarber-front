942e69d0f7d19b2ba5f0bb7981cde8a5946672db
Lucas Domingues <lucasdomi@globo.com>
2019-09-17 01:42:22 -0300

    Exibindo mensagem com toasts

--- src/store/modules/auth/sagas.js
+++ src/store/modules/auth/sagas.js
@@ -1,4 +1,5 @@
 import { takeLatest, call, put, all } from 'redux-saga/effects';
+import { toast } from 'react-toastify';
 import api from '~/services/api';
 import { signInSuccess, signFailure } from './actions';
 import history from '~/services/history';
@@ -15,7 +16,7 @@ export function* signIn({ payload }) {
     const { token, user } = response.data;
 
     if (!user.provider) {
-      console.tron.error('Usuario nao é prestador');
+      toast.error('Usuário não é prestador');
       return;
     }
 
@@ -23,6 +24,7 @@ export function* signIn({ payload }) {
 
     history.push('/dashboard');
   } catch (err) {
+    toast.error('Falha na autenticação, verifique seus dados');
     yield put(signFailure());
   }
 }
