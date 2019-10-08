a554c50c7295d9802573d63a9c70ac6cbfb248dd
Lucas Domingues <lucasdomi@globo.com>
2019-10-08 01:08:34 -0300

    Atualiza perfil e avatar

--- src/pages/Profile/index.js
+++ src/pages/Profile/index.js
@@ -1,18 +1,25 @@
 import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import { Form, Input } from '@rocketseat/unform';
-import { useSelector } from 'react-redux';
 
+import { updateProfileRequest } from '~/store/modules/user/actions';
+import AvatarInput from './AvatarInput';
 import { Container } from './styles';
 
 export default function Profile() {
+  const dispatch = useDispatch();
   const profile = useSelector(state => state.user.profile);
 
-  function handleSubmit(data) {}
+  function handleSubmit(data) {
+    dispatch(updateProfileRequest(data));
+  }
+
   return (
     <Container>
       <Form initialData={profile} onSubmit={handleSubmit}>
+        <AvatarInput name="avatar_id" />
         <Input name="name" placeholder="Nome completo" />
-        <Input name="email" placeholder="Seu endereço de email" />
+        <Input name="email" type="email" placeholder="Seu endereço de e-mail" />
 
         <hr />
 
@@ -28,10 +35,10 @@ export default function Profile() {
           placeholder="Confirmação de senha"
         />
 
-        <button type="submit"> Atualizar perfil </button>
+        <button type="submit">Atualizar perfil</button>
       </Form>
 
-      <button type="button"> Sair do GoBarber </button>
+      <button type="button">Sair do GoBarber</button>
     </Container>
   );
 }
--- src/pages/Profile/styles.js
+++ src/pages/Profile/styles.js
@@ -4,12 +4,10 @@ import { darken } from 'polished';
 export const Container = styled.div`
   max-width: 600px;
   margin: 50px auto;
-
   form {
     display: flex;
     flex-direction: column;
     margin-top: 30px;
-
     input {
       background: rgba(0, 0, 0, 0.1);
       border: 0;
@@ -18,26 +16,22 @@ export const Container = styled.div`
       padding: 0 15px;
       color: #fff;
       margin: 0 0 10px;
-
       &::placeholder {
         color: rgba(255, 255, 255, 0.7);
       }
     }
-
     span {
       color: #fb6f91;
       align-self: flex-start;
       margin: 0 0 10px;
       font-weight: bold;
     }
-
     hr {
       border: 0;
       height: 1px;
       background: rgba(255, 255, 255, 0.2);
       margin: 10px 0 20px;
     }
-
     button {
       margin: 5px 0 0;
       height: 44px;
@@ -48,20 +42,8 @@ export const Container = styled.div`
       border-radius: 4px;
       font-size: 16px;
       transition: background 0.2s;
-
       &:hover {
-        background: ${darken(0.05, '#3b9eff')};
-      }
-    }
-
-    a {
-      color: #fff;
-      margin-top: 15px;
-      font-size: 16px;
-      opacity: 0.8;
-
-      &:hover {
-        opacity: 1;
+        background: ${darken(0.03, '#3b9eff')};
       }
     }
   }
@@ -76,9 +58,8 @@ export const Container = styled.div`
     border-radius: 4px;
     font-size: 16px;
     transition: background 0.2s;
-
     &:hover {
-      background: ${darken(0.08, '#f64c75')};
+      background: ${darken(0.08, '#F64C75')};
     }
   }
 `;
